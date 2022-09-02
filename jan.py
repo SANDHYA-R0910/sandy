5c0b3f79b9b19db33604924e26453dce020b0ffb
sandhya <>
2022-09-02 12:36:34 +0530

    firstcommit

---
+++ jan.py
@@ -0,0 +1,9 @@
+import subprocess
+subprocess.run(['py','san.py'])
+# subprocess.run(['git','init'])
+subprocess.run(['git','add','.'])
+subprocess.run(['git','commit','-m','firstcommit'])
+# subprocess.run(['git','remote','add','origin','https://github.com/SANDHYA-R0910/sandy.git'])
+# subprocess.run(['git','remote'])
+# subprocess.run(['git', 'branch', '-M', 'main'])
+subprocess.run
