20a982a79ec5f21810eb3118796fdec2d54c50ad
sandhya <>
2022-09-02 12:41:04 +0530

    firstcommit

--- jan.py
+++ jan.py
@@ -1,9 +1,11 @@
+from operator import sub
 import subprocess
 subprocess.run(['py','san.py'])
 # subprocess.run(['git','init'])
 subprocess.run(['git','add','.'])
 subprocess.run(['git','commit','-m','firstcommit'])
+subprocess.run(['git', 'branch', '-M', 'main'])
 # subprocess.run(['git','remote','add','origin','https://github.com/SANDHYA-R0910/sandy.git'])
 # subprocess.run(['git','remote'])
-# subprocess.run(['git', 'branch', '-M', 'main'])
+
 subprocess.run
